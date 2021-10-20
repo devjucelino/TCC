6bb0f696c9a2f5286df927a930ee1ccaaabccdf9
devjucelino <80649895+devjucelino@users.noreply.github.com>
2021-10-20 10:08:09 -0300

    Update integracao.cpp

--- integracao.cpp
+++ integracao.cpp
@@ -40,7 +40,7 @@ public:
 
     double dblAspectRatio;
 
-    Blob(std::vector<cv::Point> _contour); //Protótipo da função
+    Blob(std::vector<cv::Point> _contour); 
 
 };
 
@@ -104,7 +104,7 @@ void camera_sensores(){
 
         while (capVideo.isOpened()) {
 
-            std::vector<Blob> blobs;
+            std::vector<Blob> blobs; // Vetor para armazenar as blobs detectadas
 
             cv::Mat imgFrame1Copy = imgFrame1.clone();
             cv::Mat imgFrame2Copy = imgFrame2.clone();
@@ -112,17 +112,23 @@ void camera_sensores(){
             cv::Mat imgDifference;
             cv::Mat imgThresh;
 
+            // Pré-tratamentos para suavização da imagem e transformação em escala de cinza
+            
             cv::cvtColor(imgFrame1Copy, imgFrame1Copy, cv::COLOR_BGR2GRAY);
             cv::cvtColor(imgFrame2Copy, imgFrame2Copy, cv::COLOR_BGR2GRAY);
 
             cv::GaussianBlur(imgFrame1Copy, imgFrame1Copy, cv::Size(5, 5), 0);
             cv::GaussianBlur(imgFrame2Copy, imgFrame2Copy, cv::Size(5, 5), 0);
 
-            cv::absdiff(imgFrame1Copy, imgFrame2Copy, imgDifference);
+            cv::absdiff(imgFrame1Copy, imgFrame2Copy, imgDifference); // Cômputo da diferença entre os frames
 
-            cv::threshold(imgDifference, imgThresh, 30, 255.0, cv::THRESH_BINARY);
+            cv::threshold(imgDifference, imgThresh, 30, 255.0, cv::THRESH_BINARY); // Threshold aplicado ao resultado da diferença entre os frames
 
             //cv::imshow("imgThresh", imgThresh);
+            
+            // O resultado do threshold obtido passa por mais um pré-processamento, em seguida são encontrados os contornos, armazenados em um vetor de vetores de pontos
+            // Após isso, são encontradas as envoltórias convexas desses pontos e analisadas cada uma de acordo com parâmetros estabelecidos para determianr se podem ser
+            // consideradas pessoas ou não.
 
             cv::Mat structuringElement3x3 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
             cv::Mat structuringElement5x5 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
@@ -181,25 +187,27 @@ void camera_sensores(){
 
             //cv::imshow("imgConvexHulls", imgConvexHulls);
 
-            imgFrame2Copy = imgFrame2.clone();          // get another copy of frame 2 since we changed the previous frame 2 copy in the processing above
+            imgFrame2Copy = imgFrame2.clone();          
 
 
-            for (auto &blob : blobs) {                                                  // for each blob
-                cv::rectangle(imgFrame2Copy, blob.boundingRect, SCALAR_RED, 2);             // draw a red box around the blob
-                cv::circle(imgFrame2Copy, blob.centerPosition, 3, SCALAR_GREEN, -1);        // draw a filled-in green circle at the center
+            for (auto &blob : blobs) {                                                  
+                cv::rectangle(imgFrame2Copy, blob.boundingRect, SCALAR_RED, 2);             
+                cv::circle(imgFrame2Copy, blob.centerPosition, 3, SCALAR_GREEN, -1);       
 
             }
 
-            filtro[contador] = blobs.size();
+            filtro[contador] = blobs.size(); // Armazena as informações das últimas 101 iterações, a quantidade de pessoas (blobs) detectadas em cada uma
 
+            // Rotina que ocorre a aproximadamente cada 15 seg na raspberry pi, é variável conforme a capacidade de processamento do harddware
+            
             if(contador == 100){
 
                 int nonzero = 0;
 
-                for(int i = 0; i < 101; i++){
-                    if(filtro[i] != 0){
-                        nonzero++;
-                    }
+                for(int i = 0; i < 101; i++){ // Aqui é feito um filtro para retirar todos os resultados que não captaram blobs nas últimas 101 iterações.
+                    if(filtro[i] != 0){       // A lógica é que se em 15 segundos foi detectado movimento ao menos uma vez, a pessoa provavelmente não terá saído de lá em 15 seg.
+                        nonzero++;           // Dessa forma é possível fazer a contabilização mais acertada, desconsiderando os momentos em que a pessoa ficou parada ou não 
+                    }                         // teve movimentos detectados nos últimos 15 segundos.
                 }
 
                 long int nao_nulos[nonzero];
@@ -214,19 +222,22 @@ void camera_sensores(){
                 }
 
 
-                int n = sizeof(nao_nulos)/sizeof(nao_nulos[0]);
-                camera_data = mostFrequent(nao_nulos,n);
-
+                int n = sizeof(nao_nulos)/sizeof(nao_nulos[0]); // Aqui é feita a chamada da função para calcular o elemento mais frequente, ou seja, excluindo-se os não-nulos
+                camera_data = mostFrequent(nao_nulos,n);      // que implicam as iterações em que não foram detectadas blobs, o número de blobs mais frequentes naquele intervalo
+                                                              // de 15 seg vai determinar quantas pessoas estavam ali de fato, atenuando as vezes em que o algoritmo detecta mais de uma blob na mesma pessoa.  
+                
+                // Aqui são feitas as chamadas para leitura dos sensores
+                
                 sensor_1 = leitura_sensor(1);
                 sensor_2 = leitura_sensor(2);
                 sensor_3 = leitura_sensor(3);
                 sensor_4 = leitura_sensor(4);
                 sensor_5 = leitura_sensor(5);
 
-                sensor_data = (sensor_1 + sensor_2 + sensor_3 + sensor_4 + sensor_5)/5;
+                sensor_data = (sensor_1 + sensor_2 + sensor_3 + sensor_4 + sensor_5)/5; // É feita uma média aritmética simples dos dados obtidos
                 //camera_data = blobs.size();
 
-                cout << "Numero de pessoas = " << camera_data << " e temperatura = " << sensor_data << endl;
+                cout << "Numero de pessoas = " << camera_data << " e temperatura = " << sensor_data << endl; //saida para tela apenas para acompanhar a execução do programa
 
                 flag = 0; //sinal para  a thread
 
@@ -236,11 +247,11 @@ void camera_sensores(){
 
             cv::imshow("imgFrame2Copy", imgFrame2Copy);
 
-                    // now we prepare for the next iteration
+                    // Preparação para a próxima iteração
 
-            imgFrame1 = imgFrame2.clone();           // move frame 1 up to where frame 2 is
+            imgFrame1 = imgFrame2.clone();           
 
-            capVideo.read(imgFrame2);               // read next frame and store it in imgFrame2
+            capVideo.read(imgFrame2);              
 
             cv::waitKey(1);
 
@@ -253,16 +264,18 @@ void camera_sensores(){
 
 
 int main(){
+    
+    //thread principal que recebe os dados de leitura dos sensores e as informações sobre a presença de pessoas
 
-    std::thread cam_sens (camera_sensores);
+    std::thread cam_sens (camera_sensores); //início da thread secundária
 
-    system("sudo modprobe w1-gpio");
+    system("sudo modprobe w1-gpio"); //chamadas ao sistema para habilitar a leitura dos sensores por meio do protocolo one-wire
 
     system("sudo modprobe w1-therm");
 
     while (1){
 
-        if (flag == 0){ //considera-se um intervalo de tempo de 10s
+        if (flag == 0){ //considera-se um intervalo de tempo de 15s
 
             time_t now;
 
@@ -270,9 +283,9 @@ int main(){
 
             ofstream fp;
 
-            fp.open("tcc_data.csv",ios::app);
+            fp.open("tcc_data.csv",ios::app); //arquivo de saída com os dados
 
-            fp << format("%d, %f, %s", camera_data, sensor_data, ctime(&now));
+            fp << format("%d, %f, %s", camera_data, sensor_data, ctime(&now)); // variáveis globais camera_data e sensor_data escritas no arquivo.
 
             fp.close();
 
@@ -336,10 +349,10 @@ float leitura_sensor(int sensor){
 
 int mostFrequent(long int arr[], int n)
 {
-    // Sort the array
+    // Organização do array
     sort(arr, arr + n);
 
-    // find the max frequency using linear traversal
+    // Encontrar o elemento que mais se repete
     int max_count = 1, res = arr[0], curr_count = 1;
     for (int i = 1; i < n; i++) {
         if (arr[i] == arr[i - 1])
@@ -353,7 +366,7 @@ int mostFrequent(long int arr[], int n)
         }
     }
 
-    // If last element is most frequent
+    // Se o último elemento é o mais frequente
     if (curr_count > max_count)
     {
         max_count = curr_count;
